9cf2416dea141b41ee338c0db5cf88f97df97dec
buggyCoder269 <Pranay.Shah1@ibm.com>
2026-06-12 16:45:35 +0530

    added ui and common components

--- axiom/src/App.tsx
+++ axiom/src/App.tsx
@@ -1,8 +1,10 @@
+import ScrollToTop from "./components/layout/ScrollToTop"
 import AppRoutes from "./routes/AppRoutes"
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <AppRoutes />
     </>
   )
